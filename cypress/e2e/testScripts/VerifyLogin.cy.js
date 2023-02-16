a492a70270a6af05ac350380266c5e6e0974c320
subhashinibollineni <subhashini.bollineni@atmecs.com>
2023-02-16 10:29:51 +0530

    Modified Class name

---
+++ cypress/e2e/testScripts/VerifyLogin.cy.js
@@ -0,0 +1,18 @@
+import LoginPage from "../../pages/LoginPage.cy";
+
+describe("Verify Login to saucelabs", () => {
+
+    let loginData
+    let productsData
+    before(() => {
+        cy.launchApplication()
+        cy.fixture("loginData").then((data) => {
+            loginData = data
+        })
+    })
+
+    it("Verify Login Test", () => {
+        let loginPage = new LoginPage();
+        loginPage.login(loginData.username, loginData.password)
+    })
+})
